366c0e320f4b37d8fff6e5734bc79d228ba7eac7
K1auez <skaue3222@gmail.com>
2024-10-18 09:31:13 -0300

    oi

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="header">
+        <div class="logo">
+            <img src="./img/LOGO-SEM-NOME.png">
+        </div>
+        <div class="links">
+            <a href="">definição</a>
+            <a href="">processo</a>
+            <a href="">avaliação</a>
+            <a href="">usos</a>
+            <a href="">recursos</a>
+            <a href="">futuro</a>
+            <a href="">faq</a>
+        </div>
+    </div>
+    <main>
+        <div class="definicao">
+            <div class="box1">
+                <p class="text-bold">O que é energia geotérmica?</p>
+                <p class="text">Energia geotérmica refere-se ao calor armazenado sob a superfície da Terra, que se origina de processos geológicos, como a decomposição de elementos radioativos e a atividade tectônica. Essa energia é uma fonte renovável, pois a Terra constantemente gera calor. Utilizar essa fonte de energia pode contribuir para a redução da dependência de combustíveis fósseis</p>
+            </div>
+            <div class="box2">
+                <img src="./img/image (1).png">
+            </div>
+        </div>
+
+        <div class="processo">
+        <p class="text-bold">Processo</p>
+            <div class="box1">
+                <img src="./img/image (2).png">
+            </div>
+            <div class="box2">
+                <p>O uso de fontes geotérmicas remonta a civilizações antigas, como os romanos e os chineses, que aproveitavam águas termais para banhos. A primeira usina geotérmica foi construída na Califórnia em 1904, marcando o início da era moderna de energia geotérmica. Desde então, a tecnologia evoluiu significativamente, com inovações que permitem a extração e conversão do calor geotérmico em eletricidade de maneira mais eficiente.</p>
+            </div>
+        </div>
+
+        <div class="avaliacao">
+            <div class="box1"></div>
+            <div class="box2"></div>
+        </div>
+
+        <div class="usos">
+            <div class="box1"></div>
+            <div class="box2"></div>
+        </div>
+
+        <div class="recursos">
+
+        </div>
+
+        <div class="futuro"></div>
+
+        <div class="faq"></div>
+
+    </main>
+
+    <div class="footer"></div>
+</body>
+</html>
